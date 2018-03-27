49ea82160a5418389fa06d3c28d6c0dcf932a48c
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-27 16:23:03 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+//Fix reference to parent theme file that breaks Snapshots - 3/26/18
 include get_template_directory() . '/includes/comment.php';
+
+//Add Support for WP Core Title Tag functionality - 3/27/18
+add_theme_support( 'title-tag' );
 
 ?>
