e8319adf178ae5cd7fa8c72548157d58e6951a95
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-03-27 16:22:02 -0600

    Update header.php

--- header.php
+++ header.php
@@ -46,7 +46,11 @@
 
     <?php endif; ?>
 
+    <?php /* Removed to fix Title Tag Issues, and add theme support for WordPress core function for title tag
+    
     <title><?php bloginfo('name') ?><?php wp_title(); ?></title>
+    
+    */ ?>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
