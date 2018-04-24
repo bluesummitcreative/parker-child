47045c82603bfcaf7522dd7a16194810f76d540b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-04-24 16:07:12 -0600

    Create design.php

---
+++ design.php
@@ -0,0 +1,19 @@
+<?php
+	/*Template Name: Design*/
+	get_header(); 
+?>
+
+	<div class="container">
+
+		<div class="theContent hasBg col-md-8">
+			PLAN DESCRIPTION HERE
+		</div>
+		<section style="margin-top:30px" class="sidebar col-md-4">
+			IMAGE OF PLAN HERE
+		</section>
+
+	</div>
+
+<?php
+	get_footer();
+?>
