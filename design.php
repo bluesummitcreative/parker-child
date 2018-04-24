7d73c4eb607f0e8caa8604616f59946f2017be54
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-04-24 16:21:54 -0600

    Update design.php

--- design.php
+++ design.php
@@ -6,8 +6,8 @@
 	<div class="container">
 
 		<div class="theContent hasBg col-md-6">
-			PLAN DESCRIPTION HERE
-			<a class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Open Modal</a>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper lacus non accumsan viverra. Sed eget nisl id sem rutrum aliquet. In non ante sem. Cras porta maximus imperdiet. Mauris.</p>
+			<p><a class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">View Plan</a> <a href="#" target="_blank" class="btn btn-primary">Download PDF</a>
 		</div>
 		<div class="theContent hasBg col-md-6">
 			<img src="http://everittconstructionco.com/wp-content/uploads/2017/07/design1-500x350.png" class="img-responsive" />
