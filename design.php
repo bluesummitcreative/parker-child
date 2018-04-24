14274f5fe7d0489e424ddbc41c57803d37d90c4d
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-04-24 16:15:42 -0600

    Update design.php

--- design.php
+++ design.php
@@ -7,7 +7,7 @@
 
 		<div class="theContent hasBg col-md-6">
 			PLAN DESCRIPTION HERE
-			<a class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</a>
+			<a class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Open Modal</a>
 		</div>
 		<div class="theContent hasBg col-md-6">
 			IMAGE OF PLAN HERE
@@ -20,25 +20,25 @@
 	</div>
 
 <!-- Modal -->
-		<div id="myModal" class="modal fade" role="dialog">
-		  <div class="modal-dialog">
-
-			<!-- Modal content-->
-			<div class="modal-content">
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<h4 class="modal-title">Modal Header</h4>
-			  </div>
-			  <div class="modal-body">
-				<p>Some text in the modal.</p>
-			  </div>
-			  <div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-			  </div>
-			</div>
-
-		  </div>
-		</div>
+		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php
 	get_footer();
